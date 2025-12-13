ff3195bc221be87ef2f9590e023343bb99add3f8
Timtamyimyams <58920123+Timtamyimyams@users.noreply.github.com>
2025-12-12 19:25:42 -0500

    Create images.js

---
+++ app/tools/images.js
@@ -0,0 +1,20 @@
+import { selectTool } from './select';
+import { scaleTool } from './scale';
+import { textTool } from './text';
+import { imageTool } from './image';
+import { drawTool } from './draw';
+import { lineTool } from './line';
+import { eraseTool } from './erase';
+
+// Add new tools here - just import and add to the object below
+export const toolDefinitions = {
+  select: selectTool,
+  scale: scaleTool,
+  text: textTool,
+  image: imageTool,
+  draw: drawTool,
+  line: lineTool,
+  erase: eraseTool,
+};
+
+export const tools = Object.values(toolDefinitions);
